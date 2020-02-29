242b2b682759aa35dce33b001d500977c25c5ba8
Pakpoomimphaiboon <61406325+Pakpoomimphaiboon@users.noreply.github.com>
2020-03-01 04:41:18 +0700

    Add files via upload

---
+++ Feb-26-Assignment/push-to-https/main.cpp
@@ -0,0 +1,165 @@
+#include "mbed.h"
+#include "https_request.h"
+
+ 
+DigitalOut led(LED1);
+InterruptIn button(USER_BUTTON);
+Thread t;
+EventQueue queue(5 * EVENTS_EVENT_SIZE);
+Serial pc(USBTX, USBRX);
+WiFiInterface *wifi;
+
+const char SSL_CA_PEM[] = "-----BEGIN CERTIFICATE-----\n"
+"MIIElDCCA3ygAwIBAgIQAf2j627KdciIQ4tyS8+8kTANBgkqhkiG9w0BAQsFADBh\n"
+"MQswCQYDVQQGEwJVUzEVMBMGA1UEChMMRGlnaUNlcnQgSW5jMRkwFwYDVQQLExB3\n"
+"d3cuZGlnaWNlcnQuY29tMSAwHgYDVQQDExdEaWdpQ2VydCBHbG9iYWwgUm9vdCBD\n"
+"QTAeFw0xMzAzMDgxMjAwMDBaFw0yMzAzMDgxMjAwMDBaME0xCzAJBgNVBAYTAlVT\n"
+"MRUwEwYDVQQKEwxEaWdpQ2VydCBJbmMxJzAlBgNVBAMTHkRpZ2lDZXJ0IFNIQTIg\n"
+"U2VjdXJlIFNlcnZlciBDQTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEB\n"
+"ANyuWJBNwcQwFZA1W248ghX1LFy949v/cUP6ZCWA1O4Yok3wZtAKc24RmDYXZK83\n"
+"nf36QYSvx6+M/hpzTc8zl5CilodTgyu5pnVILR1WN3vaMTIa16yrBvSqXUu3R0bd\n"
+"KpPDkC55gIDvEwRqFDu1m5K+wgdlTvza/P96rtxcflUxDOg5B6TXvi/TC2rSsd9f\n"
+"/ld0Uzs1gN2ujkSYs58O09rg1/RrKatEp0tYhG2SS4HD2nOLEpdIkARFdRrdNzGX\n"
+"kujNVA075ME/OV4uuPNcfhCOhkEAjUVmR7ChZc6gqikJTvOX6+guqw9ypzAO+sf0\n"
+"/RR3w6RbKFfCs/mC/bdFWJsCAwEAAaOCAVowggFWMBIGA1UdEwEB/wQIMAYBAf8C\n"
+"AQAwDgYDVR0PAQH/BAQDAgGGMDQGCCsGAQUFBwEBBCgwJjAkBggrBgEFBQcwAYYY\n"
+"aHR0cDovL29jc3AuZGlnaWNlcnQuY29tMHsGA1UdHwR0MHIwN6A1oDOGMWh0dHA6\n"
+"Ly9jcmwzLmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RDQS5jcmwwN6A1\n"
+"oDOGMWh0dHA6Ly9jcmw0LmRpZ2ljZXJ0LmNvbS9EaWdpQ2VydEdsb2JhbFJvb3RD\n"
+"QS5jcmwwPQYDVR0gBDYwNDAyBgRVHSAAMCowKAYIKwYBBQUHAgEWHGh0dHBzOi8v\n"
+"d3d3LmRpZ2ljZXJ0LmNvbS9DUFMwHQYDVR0OBBYEFA+AYRyCMWHVLyjnjUY4tCzh\n"
+"xtniMB8GA1UdIwQYMBaAFAPeUDVW0Uy7ZvCj4hsbw5eyPdFVMA0GCSqGSIb3DQEB\n"
+"CwUAA4IBAQAjPt9L0jFCpbZ+QlwaRMxp0Wi0XUvgBCFsS+JtzLHgl4+mUwnNqipl\n"
+"5TlPHoOlblyYoiQm5vuh7ZPHLgLGTUq/sELfeNqzqPlt/yGFUzZgTHbO7Djc1lGA\n"
+"8MXW5dRNJ2Srm8c+cftIl7gzbckTB+6WohsYFfZcTEDts8Ls/3HB40f/1LkAtDdC\n"
+"2iDJ6m6K7hQGrn2iWZiIqBtvLfTyyRRfJs8sjX7tN8Cp1Tm5gr8ZDOo0rwAhaPit\n"
+"c+LJMto4JQtV05od8GiG7S5BNO98pVAdvzr508EIDObtHopYJeS4d60tbvVS3bR0\n"
+"j6tJLp07kzQoH3jOlOrHvdPJbRzeXDLz\n"
+"-----END CERTIFICATE-----\n";
+ 
+const char *sec2str(nsapi_security_t sec)
+{
+    switch (sec) {
+        case NSAPI_SECURITY_NONE:
+            return "None";
+        case NSAPI_SECURITY_WEP:
+            return "WEP";
+        case NSAPI_SECURITY_WPA:
+            return "WPA";
+        case NSAPI_SECURITY_WPA2:
+            return "WPA2";
+        case NSAPI_SECURITY_WPA_WPA2:
+            return "WPA/WPA2";
+        case NSAPI_SECURITY_UNKNOWN:
+        default:
+            return "Unknown";
+    }
+}
+
+int scan_wifi() {
+    WiFiAccessPoint *ap;
+
+    printf("Scan:\n");
+    int count = wifi->scan(NULL,0);
+    if (count <= 0) {
+        printf("scan() failed with return value: %d\n", count);
+        return 0;
+    }
+
+    /* Limit number of network arbitrary to 15 */
+    count = count < 15 ? count : 15;
+    ap = new WiFiAccessPoint[count];
+    count = wifi->scan(ap, count);
+    if (count <= 0) {
+        printf("scan() failed with return value: %d\n", count);
+        return 0;
+    }
+
+    for (int i = 0; i < count; i++) {
+        printf("Network: %s secured: %s BSSID: %hhX:%hhX:%hhX:%hhx:%hhx:%hhx RSSI: %hhd Ch: %hhd\n", ap[i].get_ssid(),
+               sec2str(ap[i].get_security()), ap[i].get_bssid()[0], ap[i].get_bssid()[1], ap[i].get_bssid()[2],
+               ap[i].get_bssid()[3], ap[i].get_bssid()[4], ap[i].get_bssid()[5], ap[i].get_rssi(), ap[i].get_channel());
+    }
+    printf("%d networks available.\n", count);
+
+    delete[] ap;   
+
+    return count; 
+}
+
+
+void pressed_handler() {
+    int count;
+
+    count = scan_wifi();
+    if (count == 0) {
+        pc.printf("No WIFI APs found - can't continue further.\n");
+        return;
+    }
+
+    pc.printf("\nConnecting to %s...\n", MBED_CONF_APP_WIFI_SSID);
+    int ret = wifi->connect(MBED_CONF_APP_WIFI_SSID, MBED_CONF_APP_WIFI_PASSWORD, NSAPI_SECURITY_WPA_WPA2);
+    if (ret != 0) {
+        pc.printf("\nConnection error: %d\n", ret);
+        return;
+    }
+
+    pc.printf("Success\n\n");
+    printf("MAC: %s\n", wifi->get_mac_address());
+    printf("IP: %s\n", wifi->get_ip_address());
+    printf("Netmask: %s\n", wifi->get_netmask());
+    printf("Gateway: %s\n", wifi->get_gateway());
+    printf("RSSI: %d\n\n", wifi->get_rssi());
+
+	
+ 
+	//const char body[] = "{\"hello\":\"world\"}";
+ 
+	//HttpRequest* request = new HttpRequest(wifi, HTTP_POST, "http://www.example.com");
+	//request->set_header("Content-Type", "application/json");
+	//HttpResponse* response = request->send(body, strlen(body));
+	// if response is NULL, check response->get_error()
+ 
+	//printf("status is %d - %s\n", response->get_status_code(), response->get_status_message());
+	//printf("body is:\n%s\n", response->get_body_as_string().c_str());
+ 
+	//delete request; // also clears out the response 
+
+
+	// pass in the root certificates that you trust, there is no central CA registry in Mbed OS
+
+	
+ 
+	//NetworkInterface* wifi = /* obtain a NetworkInterface object */
+ 
+	const char body[] = "{\"hello\":\"world\"}";
+ 
+	HttpsRequest* request = new HttpsRequest(wifi, SSL_CA_PEM, HTTP_GET, "https://www.example.com/");
+	HttpResponse* response = request->send();
+	// if response is NULL, check response->get_error()
+ 
+	printf("status is %d - %s\n", response->get_status_code(), response->get_status_message());
+	printf("body is:\n%s\n", response->get_body());
+ 
+	delete request;
+
+    wifi->disconnect();
+    pc.printf("\nDone\n");    
+}
+
+
+int main() {
+    wifi = WiFiInterface::get_default_instance();
+    if (!wifi) {
+        printf("ERROR: No WiFiInterface found.\n");
+        return -1;
+    }
+
+    t.start(callback(&queue, &EventQueue::dispatch_forever));
+    button.fall(queue.event(pressed_handler));
+    pc.printf("Starting\n");
+    while(1) {
+        led = !led;
+        ThisThread::sleep_for(500);
+    }
+}
